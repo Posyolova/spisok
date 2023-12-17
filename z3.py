6bee4a153d7dfbfe02543cf29946df9d28c74bb8
Posyolova <143779118+Posyolova@users.noreply.github.com>
2023-12-17 13:42:00 +0700

    Update z3.py

--- z3.py
+++ z3.py
@@ -2,11 +2,11 @@ import random
 
 n = int(input('введите ваше число- '))
 
-lst = [random.randint(0, 25) for _ in range(10)]
+lst = [random.randint(0, 25) for _ in range(10)] # генерируем список из 10 случайных от 0 до 25
 
 print(lst)
 
 if n in lst:
-        print(lst.index(n))
+        print(lst.index(n)) #проверяем есть ли это число в списке
 else:
         print('-1')
