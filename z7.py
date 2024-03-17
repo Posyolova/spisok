5b9c393788ae47ace6095c90803755cbaebcce0d
Posyolova <143779118+Posyolova@users.noreply.github.com>
2024-03-17 22:43:54 +0700

    Update z7.py

--- z7.py
+++ z7.py
@@ -4,7 +4,7 @@ print ("начальный списoк" , lst)
 
 for i in range(10):
 
-        lst.append(random.randint(0, 100))
+        lst.append(random.randint(0, 100)) #добавляет 10 случайных чисел в диапазоне от 0 до 100 в конец списка lst и выводит список до и после добавления этих чисел.
 
 max_index = lst.index(max(numbers))# находим индкс самого большого и  самого маленкого
 min_index = lst.index(min(numbers))
